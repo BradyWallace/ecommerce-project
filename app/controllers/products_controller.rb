f7aa3982e703b94496118f27f84b01f603e2af20
Brady Wallace <bwallace2@academic.rrc.ca>
2023-11-20 15:11:39 -0600

    Added product search with category select

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    if params["category"].blank? || params["category"]["id"].blank?
+      @products = Product.all
+    else
+      category = Category.find(params["category"]["id"])
+      @products = category.products
+    end
+    @products = @products.search(params[:keywords])
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  def self.search(keywords)
+    if keywords
+      where("name LIKE ? OR descrption LIKE ?", "%#{keywords}%", "%#{keywords}%"").order('id DESC'")
+    else
+      order("id DESC")
+    end
+  end
 end
