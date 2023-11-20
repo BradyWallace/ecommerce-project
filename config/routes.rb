ecbcc6d54e73c92019d73b6ceabb4f36eed74b5a
Brady Wallace <bwallace2@academic.rrc.ca>
2023-11-20 14:51:20 -0600

    added category page bases

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
   resources :products, only: %i[index show]
+  resources :categories, only: %i[index show]
 
   root to: "products#index"
 end
