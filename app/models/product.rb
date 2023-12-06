ceea40652656dd6b1c1bd4dd0fc9e89952ad8ab2
Brady Wallace <bwallace2@academic.rrc.ca>
2023-12-06 09:48:18 -0600

    Added validation to products/categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category
 
+  validates :name, :descrption, :price, presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: { only_integer: true }
+
   def self.search(keywords)
     if keywords
       where("name LIKE ? OR descrption LIKE ?", "%#{keywords}%", "%#{keywords}%").order("id DESC")
